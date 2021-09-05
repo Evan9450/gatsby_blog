c8d3b4b416467847577c1147de2e85a133c4e4ae
Evan9450 <39398768+Evan9450@users.noreply.github.com>
2021-09-05 16:31:34 +0930

    delete main page img

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,6 @@ const IndexPage = () => (
     <Seo title="Home" />
     <h1>Welcome to Evan's Blog</h1>
     <p>This is simple Gatsby Blog created by Evan.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
   </Layout>
 )
 
